6e2a5daf7fa186a86b7021cafbbca0ab83b61b15
Johannes Formann <johannes.formann@uni-duisburg-essen.de>
2020-01-04 15:36:17 +0100

    moved networking code from hwSetup to network to make files smaller/better to understand. Names of functions/variables not yet refactored

--- src/hwSetup.hpp
+++ src/hwSetup.hpp
@@ -8,16 +8,7 @@
   void hwSetupNodeMcuCytronNmea();
   void hwSetupF9PIoBoardNmea();
   void hwSetupInitial();
-  void hwSetupNetworkAp(bool emergencyMode = true);
-  void hwSetupNetworkClient();
-  void hwSetupNetworkLan8720(uint8_t phy_addr, int power, int mdc, int mdio);
   void hwSetupWebSetup();
-  void hwSetupWebNetwork();
   char* hwSetupHardwareIdToName(uint8_t setup);
-  void hwSetupWifiMonitor( void* z );
-  void hwSetupEthernetEvent(WiFiEvent_t event);
 
-  extern bool hwSetupHasEthernet;
-  extern IPAddress hwSetupOwnAdress;
-  extern DNSServer hwSetupDnsServer;
 #endif
